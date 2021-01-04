e42feef296bacc39cae85ca37bd906b624eac9bd
Martin Hierholzer <mhier@tartaros>
2021-01-04 22:41:38 +0100

    fix for new Linux versions

--- scripts/fileRenamer.sh
+++ scripts/fileRenamer.sh
@@ -6,6 +6,7 @@ echo "so that all non-ASCII characters (like Umlauts) are changed into underscor
 echo "Continue? (Ctrl+C to abort)"
 read || exit 1
 for i in * ; do
-  mv "$i" $(echo "$i" | sed -e 's/[^A-Za-z0-9._- ]/_/g' -e 's/[äöüÄÖÜß]/_/g')
+  T=$(echo "$i" | sed -e 's/[^A-Za-z0-9._ -]/_/g' -e 's/[äöüÄÖÜß]/_/g')
+  mv "$i" "$T" 
 done
 echo "Done."
